24b574c6b56f636fd4ddb949be1f3e4f70ae5354
Stefan Lessmann <stefan.lessmann@hu-berlin.de>
2020-04-25 12:07:52 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-appStore = pd.read_csv('./data/AppleStore.csv')
-papp = pd.read_csv('./data/PrepApp.csv',index_col=False,sep='\t', encoding='utf-8')
-
-
-papp.head()
